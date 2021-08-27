fbf4982b2f9f7c934cee630f7d99a44b543fa76a
Luis Alejandro Cova Ascanio <AD780416@slad.la.mcdcorp>
2021-08-26 21:44:40 -0300

    fet: mockup and understand the grid tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <bigbox-Actividad />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import bigboxActividad from "@/components/bigboxActividad.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    bigboxActividad,
   },
 };
 </script>
---
+++ src/components/bigboxActividad.vue
@@ -0,0 +1,188 @@
+<template>
+<div class="container">
+
+    <div class="grid grid-cols-3 gap-x-5">
+
+        <div class="container">
+            <div class="container">
+                <img class="rounded-md" src="../assets/mokup_img.jpg" alt="">
+            </div>
+            <div class="flex justify-between my-3">
+                <h1 class= "font-bold" >Bodegón Nuñez</h1>
+                <img src="../assets/person_outline_black_24dp.svg" alt="">
+            </div>
+            <div class="container my-2">
+                <img src="" alt="">
+                <p class="text-left text-sm">Palermo, CABA</p>
+            </div>
+            <div class="container">
+                <p class="text-left">Un equilibrio unico entre el estilo moderno y platos tipicos porteños. Te invitan a disfrutar de esta experiencia en la comida tu living</p>
+            </div>
+            <div class="container my-2">
+                <p class="text-lg font-mediun text-left">5500 puntos</p>
+            </div>
+        </div>
+    
+        <div class="container">
+            <div class="container">
+                <img class="rounded-md" src="../assets/mokup_img.jpg" alt="">
+            </div>
+            <div class="flex justify-between my-3">
+                <h1 class= "font-bold" >Bodegón Nuñez</h1>
+                <img src="../assets/person_outline_black_24dp.svg" alt="">
+            </div>
+            <div class="container my-2">
+                <img src="" alt="">
+                <p class="text-left text-sm">Palermo, CABA</p>
+            </div>
+            <div class="container">
+                <p class="text-left">Un equilibrio unico entre el estilo moderno y platos tipicos porteños. Te invitan a disfrutar de esta experiencia en la comida tu living</p>
+            </div>
+            <div class="container my-2">
+                <p class="text-lg font-mediun text-left">5500 puntos</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="container">
+                <img class="rounded-md" src="../assets/mokup_img.jpg" alt="">
+            </div>
+            <div class="flex justify-between my-3">
+                <h1 class= "font-bold" >Bodegón Nuñez</h1>
+                <img src="../assets/person_outline_black_24dp.svg" alt="">
+            </div>
+            <div class="container my-2">
+                <img src="" alt="">
+                <p class="text-left text-sm">Palermo, CABA</p>
+            </div>
+            <div class="container">
+                <p class="text-left">Un equilibrio unico entre el estilo moderno y platos tipicos porteños. Te invitan a disfrutar de esta experiencia en la comida tu living</p>
+            </div>
+            <div class="container my-2">
+                <p class="text-lg font-mediun text-left">5500 puntos</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="container">
+                <img class="rounded-md" src="../assets/mokup_img.jpg" alt="">
+            </div>
+            <div class="flex justify-between my-3">
+                <h1 class= "font-bold" >Bodegón Nuñez</h1>
+                <img src="../assets/person_outline_black_24dp.svg" alt="">
+            </div>
+            <div class="container my-2">
+                <img src="" alt="">
+                <p class="text-left text-sm">Palermo, CABA</p>
+            </div>
+            <div class="container">
+                <p class="text-left">Un equilibrio unico entre el estilo moderno y platos tipicos porteños. Te invitan a disfrutar de esta experiencia en la comida tu living</p>
+            </div>
+            <div class="container my-2">
+                <p class="text-lg font-mediun text-left">5500 puntos</p>
+            </div>
+        </div>
+    
+        <div class="container">
+            <div class="container">
+                <img class="rounded-md" src="../assets/mokup_img.jpg" alt="">
+            </div>
+            <div class="flex justify-between my-3">
+                <h1 class= "font-bold" >Bodegón Nuñez</h1>
+                <img src="../assets/person_outline_black_24dp.svg" alt="">
+            </div>
+            <div class="container my-2">
+                <img src="" alt="">
+                <p class="text-left text-sm">Palermo, CABA</p>
+            </div>
+            <div class="container">
+                <p class="text-left">Un equilibrio unico entre el estilo moderno y platos tipicos porteños. Te invitan a disfrutar de esta experiencia en la comida tu living</p>
+            </div>
+            <div class="container my-2">
+                <p class="text-lg font-mediun text-left">5500 puntos</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="container">
+                <img class="rounded-md" src="../assets/mokup_img.jpg" alt="">
+            </div>
+            <div class="flex justify-between my-3">
+                <h1 class= "font-bold" >Bodegón Nuñez</h1>
+                <img src="../assets/person_outline_black_24dp.svg" alt="">
+            </div>
+            <div class="container my-2">
+                <img src="" alt="">
+                <p class="text-left text-sm">Palermo, CABA</p>
+            </div>
+            <div class="container">
+                <p class="text-left">Un equilibrio unico entre el estilo moderno y platos tipicos porteños. Te invitan a disfrutar de esta experiencia en la comida tu living</p>
+            </div>
+            <div class="container my-2">
+                <p class="text-lg font-mediun text-left">5500 puntos</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="container">
+                <img class="rounded-md" src="../assets/mokup_img.jpg" alt="">
+            </div>
+            <div class="flex justify-between my-3">
+                <h1 class= "font-bold" >Bodegón Nuñez</h1>
+                <img src="../assets/person_outline_black_24dp.svg" alt="">
+            </div>
+            <div class="container my-2">
+                <img src="" alt="">
+                <p class="text-left text-sm">Palermo, CABA</p>
+            </div>
+            <div class="container">
+                <p class="text-left">Un equilibrio unico entre el estilo moderno y platos tipicos porteños. Te invitan a disfrutar de esta experiencia en la comida tu living</p>
+            </div>
+            <div class="container my-2">
+                <p class="text-lg font-mediun text-left">5500 puntos</p>
+            </div>
+        </div>
+    
+        <div class="container">
+            <div class="container">
+                <img class="rounded-md" src="../assets/mokup_img.jpg" alt="">
+            </div>
+            <div class="flex justify-between my-3">
+                <h1 class= "font-bold" >Bodegón Nuñez</h1>
+                <img src="../assets/person_outline_black_24dp.svg" alt="">
+            </div>
+            <div class="container my-2">
+                <img src="" alt="">
+                <p class="text-left text-sm">Palermo, CABA</p>
+            </div>
+            <div class="container">
+                <p class="text-left">Un equilibrio unico entre el estilo moderno y platos tipicos porteños. Te invitan a disfrutar de esta experiencia en la comida tu living</p>
+            </div>
+            <div class="container my-2">
+                <p class="text-lg font-mediun text-left">5500 puntos</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="container">
+                <img class="rounded-md" src="../assets/mokup_img.jpg" alt="">
+            </div>
+            <div class="flex justify-between my-3">
+                <h1 class= "font-bold" >Bodegón Nuñez</h1>
+                <img src="../assets/person_outline_black_24dp.svg" alt="">
+            </div>
+            <div class="container my-2">
+                <img src="" alt="">
+                <p class="text-left text-sm">Palermo, CABA</p>
+            </div>
+            <div class="container">
+                <p class="text-left">Un equilibrio unico entre el estilo moderno y platos tipicos porteños. Te invitan a disfrutar de esta experiencia en la comida tu living</p>
+            </div>
+            <div class="container my-2">
+                <p class="text-lg font-mediun text-left">5500 puntos</p>
+            </div>
+        </div>
+
+    </div>
+</div>
+</template>
