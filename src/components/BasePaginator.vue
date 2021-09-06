3c6e89671f429903c9a1839a43c072b249dee5ec
Luis Alejandro Cova Ascanio <AD780416@slad.la.mcdcorp>
2021-09-06 18:37:35 -0300

    fet: Muy mala idea combinar dos framework de CSS

--- src/components/BasePaginator.vue
+++ src/components/BasePaginator.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center">
-    <v-pagination v-model="currentPage" :total-visible="8" circle>
+    <v-pagination v-model="currentPage" :length="lastPage" :total-visible="8" circle>
     </v-pagination>
   </div>
 </template>
@@ -15,10 +15,10 @@ export default {
     },
   },
 
-  updated: {
+  computed: {
     currentPage: {
       get() {
-        return parseInt(this.link.first._page, 10);;
+        return parseInt(this.link.first._page, 10);
       },
     },
     lastPage: {
--- src/views/AllActivities.vue
+++ src/views/AllActivities.vue
@@ -6,8 +6,8 @@
         grid grid-cols-1
         sm:grid-cols-2
         md:grid-cols-3
-        xl:grid-cols-4
-        2xl:grid-cols-5
+        xl:grid-cols-3
+        2xl:grid-cols-3
         gap-x-5 gap-y-6
       "
     />
