e3bfb0a4f02b3600cd861288dd755ea7c316cc6d
Luis Cova <luis.cova@bigbox.com.ar>
2022-04-06 22:55:48 -0300

    fix: change name allActivities for ViewHomee

---
+++ src/views/ViewHome.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="grid grid-rows-1">
+    <activity-card
+      v-bind:activities="activities"
+      class="
+        grid grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        xl:grid-cols-3
+        2xl:grid-cols-3
+        gap-x-5 gap-y-6
+      "
+    />
+    <base-paginator
+      v-bind:page="page"
+      v-bind:link="parseLink(link)"
+      class="mt-8"
+    />
+    <div justify-center>{{ count }}</div>
+  </div>
+</template>
+
+<script>
+import api from "@/api";
+import ActivityCard from "@/components/ActivityCard.vue";
+import BasePaginator from "@/components/BasePaginator.vue";
+
+export default {
+  name: "ViewHome",
+
+  components: { ActivityCard, BasePaginator },
+
+  data() {
+    return {
+      activities: [],
+      page: 1,
+      count: "",
+      link: "",
+    };
+  },
+
+  created() {
+    api.getLink().then((link) => (this.link = link));
+    api.getCount().then((count) => (this.count = count));
+    let page = this.page;
+    api.getPaginas(page).then((activities) => (this.activities = activities));
+  },
+  methods: {
+    parseLink(link) {
+      let parse = require("parse-link-header");
+      return parse(link);
+    },
+  },
+};
+</script>
